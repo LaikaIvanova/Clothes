10f456539ec890857398812ec64d8e9b2ff0c697
Laika Ivanova <laika.antonovna.ivanova@gmail.com>
2024-10-31 14:04:13 +0100

    - init commit

---
+++ Contents/mods/LaikasClothes/media/lua/shared/Definitions/LIC_UnderwearDefinition.lua
@@ -0,0 +1,331 @@
+-- Modded Underwear Sets
+
+UnderwearDefinition.Laika_Female_Thong_Black = {
+	chanceToSpawn = 30,
+	gender = "female",
+	top = {
+		{name="Bra_Strapless_Black", chance=50},
+		{name="Bra_Straps_Black", chance=50},
+	},
+	bottom = "LIC.Thong_Black",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Blue = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Blue", chance=50},
+		{name="LIC.Bra_Straps_Blue", chance=50},
+	},
+	bottom = "LIC.Thong_Blue",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Blue_Dark = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Blue_Dark", chance=50},
+		{name="LIC.Bra_Straps_Blue_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Blue_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Blue_Light = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Blue_Light", chance=50},
+		{name="LIC.Bra_Straps_Blue_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Blue_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Brown = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Brown", chance=50},
+		{name="LIC.Bra_Straps_Brown", chance=50},
+	},
+	bottom = "LIC.Thong_Brown",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Brown_Dark = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Brown_Dark", chance=50},
+		{name="LIC.Bra_Straps_Brown_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Brown_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Brown_Light = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Brown_Light", chance=50},
+		{name="LIC.Bra_Straps_Brown_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Brown_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Cyan = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Cyan", chance=50},
+		{name="LIC.Bra_Straps_Cyan", chance=50},
+	},
+	bottom = "LIC.Thong_Cyan",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Cyan_Dark = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Cyan_Dark", chance=50},
+		{name="LIC.Bra_Straps_Cyan_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Cyan_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Cyan_Light = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Cyan_Light", chance=50},
+		{name="LIC.Bra_Straps_Cyan_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Cyan_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Green = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Green", chance=50},
+		{name="LIC.Bra_Straps_Green", chance=50},
+	},
+	bottom = "LIC.Thong_Green",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Green_Dark = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Green_Dark", chance=50},
+		{name="LIC.Bra_Straps_Green_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Green_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Green_Light = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Green_Light", chance=50},
+		{name="LIC.Bra_Straps_Green_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Green_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Grey = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Grey", chance=50},
+		{name="LIC.Bra_Straps_Grey", chance=50},
+	},
+	bottom = "LIC.Thong_Grey",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Grey_Dark = {
+	chanceToSpawn = 20,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Grey_Dark", chance=50},
+		{name="LIC.Bra_Straps_Grey_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Grey_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Grey_Light = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Grey_Light", chance=50},
+		{name="LIC.Bra_Straps_Grey_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Grey_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Orange = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Orange", chance=50},
+		{name="LIC.Bra_Straps_Orange", chance=50},
+	},
+	bottom = "LIC.Thong_Orange",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Orange_Dark = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Orange_Dark", chance=50},
+		{name="LIC.Bra_Straps_Orange_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Orange_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Orange_Light = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Orange_Light", chance=50},
+		{name="LIC.Bra_Straps_Orange_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Orange_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Pink = {
+	chanceToSpawn = 20,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Pink", chance=50},
+		{name="LIC.Bra_Straps_Pink", chance=50},
+	},
+	bottom = "LIC.Thong_Pink",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Pink_Dark = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Pink_Dark", chance=50},
+		{name="LIC.Bra_Straps_Pink_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Pink_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Pink_Light = {
+	chanceToSpawn = 20,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Pink_Light", chance=50},
+		{name="LIC.Bra_Straps_Pink_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Pink_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Purple = {
+	chanceToSpawn = 20,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Purple", chance=50},
+		{name="LIC.Bra_Straps_Purple", chance=50},
+	},
+	bottom = "LIC.Thong_Purple",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Purple_Dark = {
+	chanceToSpawn = 20,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Purple_Dark", chance=50},
+		{name="LIC.Bra_Straps_Purple_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Purple_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Purple_Light = {
+	chanceToSpawn = 20,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Purple_Light", chance=50},
+		{name="LIC.Bra_Straps_Purple_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Purple_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Red = {
+	chanceToSpawn = 20,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Red", chance=50},
+		{name="LIC.Bra_Straps_Red", chance=50},
+	},
+	bottom = "LIC.Thong_Red",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Red_Dark = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Red_Dark", chance=50},
+		{name="LIC.Bra_Straps_Red_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Red_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Red_Light = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Red_Light", chance=50},
+		{name="LIC.Bra_Straps_Red_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Red_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_White = {
+	chanceToSpawn = 25,
+	gender = "female",
+	top = {
+		{name="Bra_Strapless_White", chance=50},
+		{name="Bra_Straps_White", chance=50},
+	},
+	bottom = "LIC.Thong_White",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Yellow = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Yellow", chance=50},
+		{name="LIC.Bra_Straps_Yellow", chance=50},
+	},
+	bottom = "LIC.Thong_Yellow",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Yellow_Dark = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Yellow_Dark", chance=50},
+		{name="LIC.Bra_Straps_Yellow_Dark", chance=50},
+	},
+	bottom = "LIC.Thong_Yellow_Dark",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Yellow_Light = {
+	chanceToSpawn = 15,
+	gender = "female",
+	top = {
+		{name="LIC.Bra_Strapless_Yellow_Light", chance=50},
+		{name="LIC.Bra_Straps_Yellow_Light", chance=50},
+	},
+	bottom = "LIC.Thong_Yellow_Light",
+}
+
+UnderwearDefinition.Laika_Female_Thong_Military = {
+	chanceToSpawn = 5,
+	gender = "female",
+	top = {
+		{name="Bra_Strapless_Black", chance=50},
+		{name="Bra_Straps_Black", chance=50},
+	},
+	bottom = "LIC.Thong_Military",
+}
---
+++ Contents/mods/LaikasClothes/media/lua/shared/Definitions/LIC_ZombiesZoneDefinition.lua
@@ -0,0 +1,5 @@
+require 'NPCs/ZombiesZoneDefinition'
+
+LIC_ZombiesZoneDefinition = ZombiesZoneDefinition or {};
+
+table.insert(ZombiesZoneDefinition.Default,{name = "LIC_Generic1", chance=1.0, gender="female"});
