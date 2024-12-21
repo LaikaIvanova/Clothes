e1a9317f8a92c3570544b714f12f6d6874f067b7
Laika Ivanova <laika.antonovna.ivanova@gmail.com>
2024-12-21 17:22:32 +0100

    - removed Build 41 files

--- Contents/mods/LaikasClothes/media/lua/shared/Definitions/LIC_ZombiesZoneDefinition.lua
+++
@@ -1,5 +0,0 @@
-require 'NPCs/ZombiesZoneDefinition'
-
-LIC_ZombiesZoneDefinition = ZombiesZoneDefinition or {};
-
-table.insert(ZombiesZoneDefinition.Default,{name = "LIC_Generic1", chance=1.0, gender="female"});
